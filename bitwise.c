713e65807ef9a129dd8941425bf1e17856fd008d
Leranbychetan <learnbychetan@gmail.com>
2024-04-13 18:06:02 +0530

    13/04

---
+++ bitwise.c
@@ -0,0 +1,16 @@
+//bitwise operator
+#include<stdio.h>
+void main()
+{
+ int a=-12;
+ int b=2;
+ int ans;
+//ans=a&b;
+//ans=a|b;
+//ans=a^b;
+//ans=a<<b;
+ans=a>>b;
+printf("ans = %d",ans);
+ 
+}
+
---
+++ compond_assignment.c
@@ -0,0 +1,15 @@
+//compond assingnment operator
+#include<stdio.h>
+void main()
+{
+ int a=14;
+ int b=5;
+ //a+=b ;//a=a+b
+ //a+=a;// a=a+a;
+ //b*=b;//b=b*b;
+ //a/=b;//a=a/b;
+ a%=b;
+ printf("value of a = %d\n",a);	//4
+ printf("value of b = %d\n",b);//5	
+}
+
